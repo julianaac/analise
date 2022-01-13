5d8013faac09f15e9c41f3ff296698ef1c0c01c1
Juliana Abreu <44270137+julianaac@users.noreply.github.com>
2022-01-13 15:38:26 -0500

    Creat teste

---
+++ test.java
@@ -0,0 +1,11 @@
+package teste;
+
+public class calculadora {
+	public static void main(String[] args) {
+		System.out.println(""+soma(2,5));
+	}
+	public static int soma (int a, int b) {
+		
+	}
+
+}
